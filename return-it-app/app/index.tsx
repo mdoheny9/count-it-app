167a1dfb77cde4687a34ce65034e328901cd9fd0
Maren Dunn <marendunn@uvic.ca>
2025-10-19 10:27:48 -0700

    saving before merging main

--- return-it-app/app/index.tsx
+++ return-it-app/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style ={styles.text}>Main Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003c70',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#f9c258',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+});
